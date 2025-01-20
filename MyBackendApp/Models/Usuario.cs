1e2da5d0428df2c4f50c79772ac8f82f8951ad03
Kintiz <juliocesar70777077@gmail.com>
2025-01-20 20:58:12 -0300

    feat: adicionar validações no modelo de usuário e melhorar o manuseio de formulários
    - Implementado  para validação no modelo de usuário.
      - Adicionadas regras para as propriedades ,  e .
      - Melhorado o tratamento de erros com mensagens detalhadas de validação.
    - Simplificado o gerenciamento de estado do formulário no React com uma função dinâmica .
      - Atualizado o manuseio de formulários para suportar múltiplos campos de forma centralizada.

--- MyBackendApp/Models/Usuario.cs
+++ MyBackendApp/Models/Usuario.cs
@@ -1,12 +1,27 @@
 namespace MyBackendApp.Models
+using System.ComponentModel.DataAnnotations;
+
 {
     public class Usuario
     {
         //modelo de dados do usuário no login
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O nome deve ter no máximo 100 caracteres.")]
         public string Nome {get;set;}
+
+        [Required(ErrorMessage = "O cpf é obrigatório.")]
+        [RegularExpression(@"^\d{11}$", ErrorMessage = "O CPF deve conter exatamente 11 dígitos.")]
         public string Cpf {get;set;}
+        
+        [Required(ErrorMessage = "O e-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "E-mail inválido.")]
         public string Email {get; set;}
+        
+        [Required(ErrorMessage = "A senha é obrigatória.")]
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "A senha deve ter entre 6 e 50 caracteres.")]
         public string Senha {get; set;}
+        
+        [Required(ErrorMessage = "a data de nascimento é obrigatória.")]
         public DateTime DataNascimento {get; set;}
     }
 }
