8475e195f5a293b4352d8a5ea0b55c59d66f275a
Julio0Cesar <juliocesar70777077@gmail.com>
2025-02-10 21:33:52 -0300

    testando e atualizando docker-compose

--- MyBackendAppTests/obj/Debug/net8.0/MyBackendAppTests.AssemblyInfo.cs
+++ MyBackendAppTests/obj/Debug/net8.0/MyBackendAppTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBackendAppTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0d3b6fc9dab8b986fd834a87d9b318c751848ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29e3556678a3e2ae26d1b61a5ab60a7cc7e0e2b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBackendAppTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBackendAppTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
